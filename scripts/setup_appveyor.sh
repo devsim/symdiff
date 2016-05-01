0db1f07dbf5528e0623d4397d84d43e940db92f0
Juan E Sanchez <juanesanchez@github>
2016-04-30 20:55:18 -0500

    windows tests

--- scripts/setup_appveyor.sh
+++ scripts/setup_appveyor.sh
@@ -5,9 +5,30 @@
 CMAKE=/cygdrive/C/Program\ Files\ \(x86\)/CMake/bin/cmake.exe
 SYMDIFF_CONFIG="appveyor"
 
-#mkdir -p win32
+#/usr/bin/mkdir -p win32
 #(cd win32; "$CMAKE" -G "Visual Studio 14" -DSYMDIFF_CONFIG=${SYMDIFF_CONFIG} ..)
 
-mkdir -p win64
+/usr/bin/mkdir -p win64
 (cd win64; "$CMAKE" -G "Visual Studio 14 Win64" -DSYMDIFF_CONFIG=${SYMDIFF_CONFIG} -DTCLMAIN=ON ..)
+
+libpath=`/usr/bin/cygpath -w $PWD/lib`
+#echo $libpath
+/usr/bin/mkdir -p bin
+/usr/bin/cat << EOF > bin/symdiff.bat
+@setlocal
+@echo off
+SET PYTHONPATH=$libpath;%PYTHONPATH%
+python %*
+EOF
+/usr/bin/chmod +x bin/symdiff.bat
+
+# TCLLIBPATH must always use forward slashes
+libpath=`/usr/bin/cygpath -m $PWD/lib`
+/usr/bin/cat << EOF > bin/symdiff_tcl.bat
+@setlocal
+@echo off
+SET TCLLIBPATH="$libpath" %TCLLIBPATH%
+tclsh %*
+EOF
+/usr/bin/chmod +x bin/symdiff_tcl.bat
 
--- scripts/setup_windows.sh
+++ scripts/setup_windows.sh
@@ -5,32 +5,30 @@
 CMAKE=/cygdrive/C/Program\ Files\ \(x86\)/CMake/bin/cmake.exe
 SYMDIFF_CONFIG="win32"
 
-mkdir -p win32
+/usr/bin/mkdir -p win32
 (cd win32; "$CMAKE" -G "Visual Studio 14" -DSYMDIFF_CONFIG=${SYMDIFF_CONFIG} ..)
 
-mkdir -p win64
+/usr/bin/mkdir -p win64
 (cd win64; "$CMAKE" -G "Visual Studio 14 Win64" -DSYMDIFF_CONFIG=${SYMDIFF_CONFIG} ..)
 
-#libpath=`cygpath -w $PWD/lib`
-#PYTHON_BIN="C:\Python27\python.exe"
-##echo $libpath
-#mkdir -p bin
-#cat << EOF > bin/symdiff.bat
-#@setlocal
-#@echo off
-#SET PATH="$libpath";%PATH%
-#SET PYTHONPATH="$libpath";%PYTHONPATH%
-#${PYTHON_BIN} %*
-#EOF
-#chmod +x bin/symdiff.bat
-##
-##cat << EOF > bin/symdiff_tcl
-###!/bin/bash
-##set -e
-##progname="\$0"
-##curdir=\`dirname "\$progname"\`
-##export TCLLIBPATH=\${curdir}/../lib
-##${TCL_BIN} \$*
-##EOF
-##chmod +x bin/symdiff_tcl
-##
+libpath=`/usr/bin/cygpath -w $PWD/lib`
+#echo $libpath
+/usr/bin/mkdir -p bin
+/usr/bin/cat << EOF > bin/symdiff.bat
+@setlocal
+@echo off
+SET PYTHONPATH=$libpath;%PYTHONPATH%
+python %*
+EOF
+/usr/bin/chmod +x bin/symdiff.bat
+
+# TCLLIBPATH must always use forward slashes
+libpath=`/usr/bin/cygpath -m $PWD/lib`
+/usr/bin/cat << EOF > bin/symdiff_tcl.bat
+@setlocal
+@echo off
+SET TCLLIBPATH="$libpath" %TCLLIBPATH%
+tclsh %*
+EOF
+/usr/bin/chmod +x bin/symdiff_tcl.bat
+
