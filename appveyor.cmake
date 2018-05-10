92bad751e03dc4e0f9620a8809105ae7998aed07
Juan E. Sanchez <juan@tcad.com>
2018-05-10 17:09:14 -0500

    use anaconda Tcl

--- appveyor.cmake
+++ appveyor.cmake
@@ -2,21 +2,14 @@
 # http://creativecommons.org/publicdomain/zero/1.0/
 #SET(PYTHON_BIN="/usr/bin/python")
 
-IF (${CMAKE_SIZEOF_VOID_P} MATCHES 4)
-#32 bit build
-SET(TCL_INCLUDE "c:/Tcl/include")
-SET(TCL_ARCHIVE "c:/Tcl/lib/tclstub85.lib")
-SET(TCL_BIN     "c:/Tcl/bin/tclsh.exe")
-SET(PYTHON_INCLUDE "c:/Miniconda-x64/include")
-SET(PYTHON_ARCHIVE "c:/Miniconda-x64/libs/python27.lib")
-ELSE (${CMAKE_SIZEOF_VOID_P} MATCHES 4)
 #64 bit build
-SET(TCL_INCLUDE "c:/Tcl/include")
-SET(TCL_ARCHIVE "c:/Tcl/lib/tclstub85.lib")
-SET(TCL_BIN     "c:/Tcl/bin/tclsh.exe")
+#SET(TCL_INCLUDE "c:/Tcl/include")
+#SET(TCL_ARCHIVE "c:/Tcl/lib/tclstub85.lib")
+SET(TCL_INCLUDE "c:/Miniconda-x64/Library/include")
+SET(TCL_ARCHIVE "c:/Miniconda-x64/Library/lib/tcl86t.lib")
+SET(TCL_BIN     "c:/Miniconda-x64/Library/bin/tclsh.exe")
 SET (PYTHON_ARCHIVE C:/Miniconda-x64/libs/python27.lib)
 SET (PYTHON_INCLUDE C:/Miniconda-x64/include)
-ENDIF (${CMAKE_SIZEOF_VOID_P} MATCHES 4)
 
 SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /fp:strict /EHsc")
 SET (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /SAFESEH:NO")
