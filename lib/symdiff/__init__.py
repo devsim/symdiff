0ad8f1667f8a7a1dbf75ddb173f9f0ec1022ee78
Juan E. Sanchez <juan@tcad.com>
2019-05-07 23:44:28 -0500

    import version string

--- lib/symdiff/__init__.py
+++ lib/symdiff/__init__.py
@@ -1,9 +1,10 @@
 
 import sys
 
-if sys.version_info[0] < 3:
-  from symdiff_py2 import *
-else:
+if sys.version_info[0] == 3:
   from .symdiff_py3 import *
+  from .symdiff_py3 import __version__
+else:
+  raise ImportError('module not available for Python %d.%d please contact technical support' % sys.version_info[0:2])
 
 
