c0defbf286654589593f768934fa9b651fde2051
Juan E. Sanchez <juan@tcad.com>
2023-01-27 20:49:23 -0600

    arm64 packaging

---
+++ bdist_wheel/fix_macos_arch.py
@@ -0,0 +1,22 @@
+import wheel
+import wheel.bdist_wheel
+import sys
+
+try:
+    plat = wheel.bdist_wheel.get_platform('')
+except TypeError:
+    #TypeError: get_platform() takes 0 positional arguments but 1 was given
+    plat = wheel.bdist_wheel.get_platform()
+#
+#print(plat)
+
+for i in 'x86_64', 'arm64':
+  p = plat.find(i)
+  if p != -1:
+      prefix = plat[0:p]
+      break
+
+print(prefix+ sys.argv[1])
+#
+#
+#
--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -40,23 +40,26 @@ else
 fi
 
 
-#minimal conda environments to prevent linking against the wrong libraries
-if [ "${1}" = "gcc" ]
-then
-# now opt for explicit dll load of mkl
-conda create  -y --name python3_symdiff_build python=3 cmake nomkl
-fi
+##minimal conda environments to prevent linking against the wrong libraries
+#if [ "${1}" = "gcc" ]
+#then
+## now opt for explicit dll load of mkl
+#conda create  -y --name python3_symdiff_build python=3 cmake nomkl
+#fi
+#
+##This version does not use pardiso
+#if [ "${1}" = "clang" ]
+#then
+#conda create  -y --name python3_symdiff_build python=3 cmake nomkl
+#fi
+#source activate python3_symdiff_build
 
-#This version does not use pardiso
-if [ "${1}" = "clang" ]
-then
-conda create  -y --name python3_symdiff_build python=3 cmake nomkl
-fi
-source activate python3_symdiff_build
-
-export PYTHON3_BIN=python
-export PYTHON3_INCLUDE=$(python -c "from sysconfig import get_paths as gp; print(gp()['include'])")
+export PYTHON3_BIN=/usr/local/bin/python3
+export PIP_BIN=/usr/local/bin/pip3
+${PIP_BIN} install wheel
+export PYTHON3_INCLUDE=$(${PYTHON3_BIN} -c "from sysconfig import get_paths as gp; print(gp()['include'])")
 export PYTHON3_ARCHIVE=""
+
 
 # SYMDIFF build
 if [ "${1}" = "gcc" ]
@@ -64,12 +67,25 @@ then
 (bash scripts/symdiff_macos.sh && cd osx_release && make -j4 && make test)
 elif [ "${1}" = "clang" ]
 then
-(bash  scripts/symdiff_macos.sh && cd osx_release && make -j4 && make test)
+if [ ! -z "${ARCH}" ]
+then
+  export ARCH_ARG="-DCMAKE_OSX_ARCHITECTURES=\"${ARCH}\""
+fi;
+#TODO test arch
+(bash  scripts/symdiff_macos.sh && cd osx_release && make -j4)
+if [ $ARCH == $(uname -m) ]
+then
+(cd osx_release && make test)
+fi
+#(bash  scripts/symdiff_macos.sh && cd osx_release && make -j4 && make test)
 fi
 
 (rsync -avP --exclude __pycache__ lib/symdiff bdist_wheel/)
 (rsync -avP --exclude __pycache__ LICENSE NOTICE README.md examples doc bdist_wheel)
-(cd bdist_wheel && pip wheel .)
+FULL_PLAT_NAME=$(${PYTHON3_BIN} bdist_wheel/fix_macos_arch.py ${ARCH})
+echo PACKAGING $FULL_PLAT_NAME
+(cd bdist_wheel &&  perl -p -i -e "s/^#plat-name.*/plat-name = ${FULL_PLAT_NAME}/" setup.cfg)
+(cd bdist_wheel && ${PIP_BIN} wheel .)
 (mv bdist_wheel/*.whl .)
 
 
