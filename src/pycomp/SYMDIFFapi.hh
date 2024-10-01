8a5ca475f2971d7c81ae2de1722df3c90c806a4a
Juan E. Sanchez <juan@tcad.com>
2024-09-30 23:24:11 -0500

    add more todo functions

--- src/pycomp/SYMDIFFapi.hh
+++ src/pycomp/SYMDIFFapi.hh
@@ -12,8 +12,6 @@ SPDX-License-Identifier: Apache-2.0
 /** this does not need to be extern "C" if handled from a C++ based implementation */
 extern "C" DLL_PUBLIC void SYMDIFFobj_free(void *);
 
-extern "C" DLL_PUBLIC void * SYMDIFF_var(const char *);
-
 extern "C" DLL_PUBLIC bool SYMDIFF_get_status(void *);
 
 extern "C" DLL_PUBLIC const char * SYMDIFF_get_string(void *);
@@ -21,6 +19,14 @@ extern "C" DLL_PUBLIC const char * SYMDIFF_get_string(void *);
 extern "C" DLL_PUBLIC const char * SYMDIFF_get_error(void *);
 
 extern "C" DLL_PUBLIC void * SYMDIFF_parse(const char *);
+
+/**
+extern "C" DLL_PUBLIC void * SYMDIFF_apply_func(const char *func, void **args, numargs);
+extern "C" DLL_PUBLIC void * SYMDIFF_clone(void *);
+extern "C" DLL_PUBLIC void * SYMDIFF_simplify(void *);
+extern "C" DLL_PUBLIC void * SYMDIFF_model(const char *);
+extern "C" DLL_PUBLIC void * SYMDIFF_var(const char *);
+*/
 
 #endif
 
